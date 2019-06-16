83463ae58e97a88197495c37273608009037a150
kal247 <kal247@gmail.com>
2019-06-16 23:54:05 +0200

    initial commit

---
+++ lib/App/uricolor.pm
@@ -0,0 +1,109 @@
+package App::uricolor;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+App::uricolor - The great new App::uricolor!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use App::uricolor;
+
+    my $foo = App::uricolor->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+jul, C<< <kal247 at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-app-uricolor at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-uricolor>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc App::uricolor
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=App-uricolor>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-uricolor>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/App-uricolor>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/App-uricolor>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2019 by jul.
+
+This is free software, licensed under:
+
+  The GNU General Public License, Version 3, June 2007
+
+
+=cut
+
+1; # End of App::uricolor
